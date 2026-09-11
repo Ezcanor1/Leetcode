688306b004e751aa14bd172c16ec56a49b0f6b59
Sany Das <107028023+Ezcanor1@users.noreply.github.com>
2026-09-11 20:51:12 +0530

    Time: 10 ms (99.83%) | Memory: 20.7 MB (35.13%) - LeetSync

--- My_Leet/2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
+++ My_Leet/2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.py
@@ -4,18 +4,8 @@ class Solution(object):
         b=nums.index(min(nums))+1
         if len(nums)<=1:
             return 1
-        print(f,b)
-        print(max(f,b))
-        print(len(nums))
-        print((b+len(nums)-f))+1
-        print(f+(len(nums)-b)+1)
-        print(min(f+(len(nums)-b)+1,b+(len(nums)-f)+1))
-        # if max(f,b)<(f+len(nums)-b):
-        #     return max(f,b)
-        
-        print(max(len(nums)-f,len(nums)-b))+1
-        # print(min(len(nums)-f),len(nums)-b)
         return min(max(len(nums)-f,len(nums)-b)+1,max(f,b),f+(len(nums)-b)+1,b+(len(nums)-f)+1)
+        
             
 
         
